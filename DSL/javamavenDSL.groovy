27acd981a7842fa2191c97a90b5b350ea0b2fc00
Agusto87 <104561097+Agusto87@users.noreply.github.com>
2022-04-28 11:28:52 +0200

    Update javamavenDSL.groovy

--- DSL/javamavenDSL.groovy
+++ DSL/javamavenDSL.groovy
@@ -1,7 +1,7 @@
 job('Java Maven App DSL') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
+        git('https://github.com/Agusto87/simple-java-maven-app.git', 'master') { node ->
             node / gitConfigName('aureliosanchezfuentes')
             node / gitConfigEmail('aureliosanchezfuentes@gmail.com')
         }
